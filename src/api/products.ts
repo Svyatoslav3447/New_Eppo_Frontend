7c015630570ea5c116c570405fca88857d8abdd7
Svyatoslav3447 <yurkiv.svyatoslav23@gmail.com>
2026-03-09 23:39:13 +0200

    Update products.ts

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,30 +1,21 @@
 import { api } from "./axios";
 export { api };
 
-export interface Product {
-  id: number;
-  sku: string;
-  price_usd: number;
-  price_grn: number;
-  created_at?: string;
-
-  category: { id: number; name: string };
-  subcategory?: { id: number; name: string };
-  type?: { id: number; name: string };
-  totalSold?: number;
-  is_hidden?: boolean;
-
-  parameters?: {
-    parameter: { id: number; name: string };
-    values: string[]; // <- тепер масив значень
-  }[]; // <-- додано
+export interface ProductsResponse {
+  data: Product[];
+  pagination: {
+    total: number;
+    page: number;
+    perPage: number;
+    totalPages: number;
+  };
 }
 
-// Всі продукти
-export const getProducts = async (): Promise<Product[]> => {
-  const token = localStorage.getItem("token"); // JWT
-  const res = await api.get<Product[]>("/products", {
+export const getProducts = async (page = 1, limit = 8): Promise<ProductsResponse> => {
+  const token = localStorage.getItem("token");
+  const res = await api.get<ProductsResponse>("/products", {
     headers: token ? { Authorization: `Bearer ${token}` } : undefined,
+    params: { page, limit },
   });
   return res.data;
 };
@@ -52,3 +43,4 @@ export const deleteProduct = (id: number) => {
     headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
   });
 };
+
